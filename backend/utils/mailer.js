51c1fd169c1fb507dff3f5016929573e1f902f0d
cephas001 <okodughapeter58@gmail.com>
2026-03-21 22:37:16 +0100

    fix: force nodemailer to use IPv4 to prevent ENETUNREACH on Render

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -6,6 +6,9 @@ const Jimp = require("jimp");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  port: 467,
+  family: 4,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
